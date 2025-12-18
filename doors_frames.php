321bfe0b256533463a48e8657d14ffe50185cac7
iztokinvest <90605919+iztokinvest@users.noreply.github.com>
2025-12-18 10:47:01 +0200

    1.25.4

--- doors_frames.php
+++ doors_frames.php
@@ -3,7 +3,7 @@
 Plugin Name: Doors Frames
 Plugin URI: https://github.com/iztokinvest/doors_frames
 Description: Цени на каси.
-Version: 1.25.3
+Version: 1.25.4
 Author: Martin Mladenov
 GitHub Plugin URI: https://github.com/iztokinvest/doors_frames
 GitHub Branch: main
